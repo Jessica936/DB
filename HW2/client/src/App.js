363a691c17298c8453ddff33770b2fe4cfbb6c89
Jessica936 <lin.jessica69@gmail.com>
2023-09-30 18:01:52 +0800

    123

---
+++ HW2/client/src/App.js
@@ -0,0 +1,179 @@
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
+
+function App() {
+  const [buyerName, setName] = useState("");
+  const [itemID, setItemID] = useState("");
+  const [itemName, setItemName] = useState("");
+
+  const [buyerList, setBuyerList] = useState([]);
+
+  const addBuyer = () => {
+    Axios.post("http://localhost:3001/create", {
+      buyName: buyerName,
+      itemID: itemID,
+      itemName: itemName,
+
+    }).then(() => {
+      setBuyerList([
+        ...buyerList,
+        {
+          buyName: buyerName,
+          itemID: itemID,
+          itemName: itemName,
+        },
+      ]);
+    });
+  };
+
+  const getBuyer = () => {
+    Axios.get("http://localhost:3001/employees").then((response) => {
+      setBuyerList(response.data);
+    });
+  };
+
+  const updateBuyeritemID = (id) => {
+    Axios.put("http://localhost:3001/update", { itemID: itemID, id: id }).then(
+      (response) => {
+        setBuyerList(
+          buyerList.map((val) => {
+            return val.id == id
+              ? {
+                  id: val.id,
+                  buyName: val.buyerName,
+                  itemID: itemID,
+                  itemName: val.itemName,
+                }
+              : val;
+          })
+        );
+      }
+    );
+  };
+
+  const deleteBuyer = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+      setBuyerList(
+        buyerList.filter((val) => {
+          return val.id != id;
+        })
+      );
+    });
+  };
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Buyer Name:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Item ID:</label>
+        <select
+          onChange={(event) => {
+            setItemID(event.target.value);
+          }}
+        >
+          <option value="1">NO.1</option>
+          <option value="2">NO.2</option>
+          <option value="3">NO.3</option>
+          <option value="4">NO.4</option>
+          <option value="5">NO.5</option>
+          <option value="6">NO.6</option>
+          <option value="7">NO.7</option>
+          <option value="8">NO.8</option>
+          <option value="9">NO.9</option>
+          <option value="10">NO.10</option>
+          <option value="11">NO.11</option>
+          <option value="12">NO.12</option>
+          <option value="13">NO.13</option>
+          <option value="14">NO.14</option>
+          <option value="15">NO.15</option>
+        </select>
+        <label>Item Name:</label>
+        <select
+          onChange={(event) => {
+            setItemName(event.target.value);
+          }}
+        >
+          <option value="Water bottle">Water bottle</option>
+          <option value="Sweater">Sweater</option>
+          <option value="Blouse">Blouse</option>
+          <option value="Nike Court Legacy Lift">Nike Court Legacy Lift</option>
+          <option value="Nike Air Force 1 Shadow">Nike Air Force 1 Shadow</option>
+          <option value="Smartphone X">Smartphone X</option>
+          <option value="Running Shoes">Running Shoes</option>
+          <option value="Garden Hose">Garden Hose</option>
+          <option value="Laptop Pro">Laptop Pro</option>
+          <option value="Denim Jeans">Hybrid</option>
+          <option value="4K TV">4K TV</option>
+          <option value="Coffee Maker">Coffee Maker</option>
+          <option value="Winter Coat">Winter Coat</option>
+          <option value="Bluetooth Headphones">Bluetooth Headphones</option>
+          <option value="Patio Furniture Set">Patio Furniture Set</option>
+        </select>
+        <button onClick={addBuyer}>Add Buyer</button>
+      </div>
+      <div className="buyer">
+        <button onClick={getBuyer}>Show Buyer</button>
+
+        {buyerList.map((val, key) => {
+          return (
+            <div className="buyer" key={key}>
+              <div>
+                <h3>Buyer Name: {val.buyerName}</h3>
+                <h3>Item ID: {val.itemID}</h3>
+                <h3>Item Name: {val.itemName}</h3>
+              </div>
+              <div>
+              <select
+                onChange={(event) => {
+                  setItemID(event.target.value);
+                }}
+              >
+                <option value="1">NO.1</option>
+                <option value="2">NO.2</option>
+                <option value="3">NO.3</option>
+                <option value="4">NO.4</option>
+                <option value="5">NO.5</option>
+                <option value="6">NO.6</option>
+                <option value="7">NO.7</option>
+                <option value="8">NO.8</option>
+                <option value="9">NO.9</option>
+                <option value="10">NO.10</option>
+                <option value="11">NO.11</option>
+                <option value="12">NO.12</option>
+                <option value="13">NO.13</option>
+                <option value="14">NO.14</option>
+                <option value="15">NO.15</option>
+              </select>
+              <button
+                  onClick={() => {
+                    updateBuyeritemID(val.id);
+                  }}
+                >
+                  {" "}
+                  Update
+                </button>
+
+                <button
+                  onClick={() => {
+                    deleteBuyer(val.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ HW2/server/index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "MyS22157988",
+  database: "erd",
+});
+
+app.post("/create", (req, res) => {
+  const buyerName = req.body.buyerName;
+  const itemID = req.body.itemID;
+  const itemName = req.body.itemName;
+
+  db.query(
+    "INSERT INTO erd.buyer (buyerName, itemID, itemName) VALUES (?,?,?)",
+    [buyerName, itemID, itemName],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
+app.get("/buyer", (req, res) => {
+  db.query("SELECT * FROM erd.buyer", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/update", (req, res) => {
+  const id = req.body.itemID;
+  const itemName= req.body.itemName;
+  db.query(
+    "UPDATE buyer SET item = ? WHERE id = ?",
+    [itemName, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM buyer WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Yey, your server is running on port 3001");
+});

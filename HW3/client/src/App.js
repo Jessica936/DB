18b14a5280406aa086a2ad47962a5113d9eeffbf
Jessica936 <lin.jessica69@gmail.com>
2023-10-23 12:13:32 +0800

    hw3

---
+++ HW3/client/src/App.js
@@ -0,0 +1,229 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+function App() {
+  const [buyerName, setName] = useState("");
+  const [buyerCity, setCity] = useState("");
+  // const [itemID, setItemID] = useState("");
+  // const [itemName, setItemName] = useState("");
+
+  const [buyerList, setBuyerList] = useState([]);
+  // const [itemList, setItemList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState(""); // State for the search query
+  const [searchResults, setSearchResults] = useState([]); // State to store search results
+
+  // useEffect(() => {
+  //   getBuyerList();
+  // }, []);
+
+  const getBuyer = () => {
+    Axios.get("http://localhost:3001/buyer").then((response) => {
+      setBuyerList(response.data);
+    });
+  };
+
+  const addBuyer = () => {
+    Axios.post("http://localhost:3001/createBuyer", {
+      buyerName: buyerName,
+      buyerCity: buyerCity,
+
+    }).then(() => {
+      setBuyerList([
+        ...buyerList,
+        {
+          buyerName: buyerName,
+          buyerCity: buyerCity,
+        },
+      ]);
+    });
+  };
+
+  // Assuming you've already imported Axios somewhere in your code
+
+  const updateBuyer = (id) => {
+    console.log("Updating buyer with id:", id);
+  
+    const newBuyerName = prompt("Enter new Buyer name:");
+    if (newBuyerName !== null) {
+      Axios.put(`http://localhost:3001/updateBuyer/${id}`, {
+        buyerName: newBuyerName,
+      })
+        .then(() => {
+          getBuyer(); // Refresh the user list
+        })
+        .catch((error) => {
+          console.error("Error updating buyer:", error);
+        });
+    }
+  };
+  
+
+
+  const deleteBuyer = (id) => {
+    Axios.delete(`http://localhost:3001/deleteBuyer/${id}`).then((response) => {
+      getBuyer();
+    });
+  };
+
+  // const searchBuyer = () => {
+  //   Axios.get(`http://localhost:3001/searchBuyer?search=${searchQuery}`).then(
+  //     (response) => {
+  //       setSearchResults(response.data);
+  //     }
+  //   );
+  // };
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Buyer Name:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+
+        <label>Buyer City:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCity(event.target.value);
+          }}
+        />
+
+        {/* <label>Item ID:</label>
+        <select
+          onChange={(event) => {
+            setItemID(event.target.value);
+          }}
+        >
+          <option value="1">NO.1</option>
+          <option value="2">NO.2</option>
+          <option value="3">NO.3</option>
+          <option value="4">NO.4</option>
+          <option value="5">NO.5</option>
+          <option value="6">NO.6</option>
+          <option value="7">NO.7</option>
+          <option value="8">NO.8</option>
+          <option value="9">NO.9</option>
+          <option value="10">NO.10</option>
+          <option value="11">NO.11</option>
+          <option value="12">NO.12</option>
+          <option value="13">NO.13</option>
+          <option value="14">NO.14</option>
+          <option value="15">NO.15</option>
+        </select>
+        <label>Item Name:</label>
+        <select
+          onChange={(event) => {
+            setItemName(event.target.value);
+          }}
+        >
+          <option value="Water bottle">Water bottle</option>
+          <option value="Sweater">Sweater</option>
+          <option value="Blouse">Blouse</option>
+          <option value="Nike Court Legacy Lift">Nike Court Legacy Lift</option>
+          <option value="Nike Air Force 1 Shadow">Nike Air Force 1 Shadow</option>
+          <option value="Smartphone X">Smartphone X</option>
+          <option value="Running Shoes">Running Shoes</option>
+          <option value="Garden Hose">Garden Hose</option>
+          <option value="Laptop Pro">Laptop Pro</option>
+          <option value="Denim Jeans">Hybrid</option>
+          <option value="4K TV">4K TV</option>
+          <option value="Coffee Maker">Coffee Maker</option>
+          <option value="Winter Coat">Winter Coat</option>
+          <option value="Bluetooth Headphones">Bluetooth Headphones</option>
+          <option value="Patio Furniture Set">Patio Furniture Set</option>
+        </select> */}
+        <button onClick={addBuyer}>Add Buyer</button>
+        {/* <button onClick={addItem}>Add Item</button> */}
+      </div>
+      {/* <div className="search">
+                <label>Search Buyer:</label>
+                <input
+                  type="text"
+                  onChange={(event) => {
+                    setSearchQuery(event.target.value);
+                  }}
+                />
+                <button onClick={searchBuyer}>Search</button>
+              </div> */}
+
+              {/* Display search results */}
+              {/* <div className="search-results">
+                {searchResults.map((result, index) => {
+                  return (
+                    <div className="search-result" key={index}>
+                      <p>ItemID: {result.itemID}</p>
+                      <p>Item Name: {result.itemName}</p>
+                    </div>
+                  );
+                })}
+              </div> */}
+      <div className="buyer">
+        <button onClick={getBuyer}>Show Buyer</button>
+        {/* <button onClick={getItem}>Show Item</button> */}
+
+        {buyerList.map((val, key) => {
+          return (
+            <div className="buyer" key={key}>
+              <div>
+                <h3>Buyer ID: {val._id}</h3>
+                <h3>Buyer Name: {val.buyerName}</h3>
+                <h3>Buyer City: {val.buyerCity}</h3>
+                {/* <h3>Item ID: {val.itemID}</h3>
+                <h3>Item Name: {val.itemName}</h3> */}
+              </div>
+              <div>
+                {/* <select
+                onChange={(event) => {
+                  setItemID(event.target.value);
+                }}
+              >
+                <option value="1">NO.1</option>
+                <option value="2">NO.2</option>
+                <option value="3">NO.3</option>
+                <option value="4">NO.4</option>
+                <option value="5">NO.5</option>
+                <option value="6">NO.6</option>
+                <option value="7">NO.7</option>
+                <option value="8">NO.8</option>
+                <option value="9">NO.9</option>
+                <option value="10">NO.10</option>
+                <option value="11">NO.11</option>
+                <option value="12">NO.12</option>
+                <option value="13">NO.13</option>
+                <option value="14">NO.14</option>
+                <option value="15">NO.15</option>
+              </select> */}
+                <button
+                  onClick={() => {
+                    updateBuyer(val._id);
+                  }}
+                >
+                  {" "}
+                  Update
+                </button>
+
+                <button
+                  onClick={() => {
+                    deleteBuyer(val._id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+            
+          );
+        })}
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
+
---
+++ HW3/server/index.js
@@ -0,0 +1,137 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+// const ObjectId = mongoose.Types.ObjectId;
+
+app.use(cors());
+app.use(express.json());
+
+// 連接至 MongoDB
+mongoose.connect("mongodb+srv://JessicaLin:Mongo22157988@cluster0.7jzoted.mongodb.net/buyer", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("MongoDB 連接錯誤:", error);
+});
+
+db.once("open", () => {
+  console.log("已成功連接至 MongoDB");
+});
+
+const buyerSchema = new mongoose.Schema({
+  buyerName: String,
+  buyerCity: String,
+});
+
+
+const Buyer = mongoose.model("Buyer", buyerSchema);
+
+
+app.post("/createBuyer", async (req, res) => {
+  const buyerName = req.body.buyerName;
+  const buyerCity = req.body.buyerCity;
+
+  try {
+    const buyer = new Buyer({
+      buyerName,
+      buyerCity,
+    });
+    await buyer.save();
+    res.send("Buyer Inserted");
+  } catch (error) {
+    console.error("創建買家時發生錯誤:", error);
+    res.status(500).send("Error creating course");
+  }
+});
+
+
+app.get("/buyer", async (req, res) => {
+  try {
+    const buyer = await Buyer.find({});
+    res.send(buyer);
+  } catch (error) {
+    console.error("獲取買家列表時發生錯誤:", error);
+    res.status(500).send("Error fetching buyer");
+  }
+});
+
+
+// app.post("/createItem", (req, res) => {
+//   const itemID = req.body.itemID;
+//   const itemName = req.body.itemName;
+
+//   db.query(
+//     "INSERT INTO item (itemID, itemName) VALUES (?,?)",
+//     [itemID, itemName],
+//     (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.send("Values Inserted");
+//       }
+//     }
+//   );
+// });
+
+// app.get("/item", (req, res) => {
+//   db.query("SELECT * FROM item", (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(result);
+//     }
+//   });
+// });
+
+app.put("/updateBuyer/:id", (req, res) => {
+  const id = req.params.id;
+  const buyerName = req.body.buyerName;
+  
+  Buyer.findByIdAndUpdate(id, { buyerName }, { new: true })
+    .then((updatedBuyer) => {
+      if (!updatedBuyer) {
+        return res.status(404).send("Buyer not found");
+      }
+      res.send(updatedBuyer);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error updating Buyer");
+    });
+});
+
+
+app.delete("/deleteBuyer/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    // 不需要將字符串轉換為ObjectId，因為Mongoose可以處理
+    await Buyer.findByIdAndRemove(id);
+    res.send("Buyer Deleted");
+  } catch (error) {
+    console.error("刪除買家時發生錯誤:", error);
+    res.status(500).send("Error deleting buyer");
+  }
+});
+
+// app.get("/searchBuyer", (req, res) => {
+//   const searchQuery = req.query.search || "";
+
+//   const sqlQuery = `
+//   SELECT erd.buyer.buyerName, erd.buyer_item.itemID, erd.item.itemName
+//   FROM ((erd.buyer_item
+//   INNER JOIN erd.buyer ON erd.buyer_item.buyerID = erd.buyer.buyerID)
+//   INNER JOIN erd.item ON erd.buyer_item.itemID = erd.item.itemID)
+//   WHERE erd.buyer.buyerName LIKE ?
+//   OR erd.buyer_item.itemID LIKE ?
+//   OR erd.item.itemName LIKE ?
+//   `;
+
+app.listen(3001, () => {
+  console.log("Yey, your server is running on port 3001");
+});
